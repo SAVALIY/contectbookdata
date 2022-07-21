d388f3b1705fd3e1891ffed3230ca1672436d427
SAVALIY <anuragsavaliya006@gmail.com>
2022-07-21 07:23:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:contectbookdata/viewpage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,home: viewpage(),));
+
+}
